e2e2c0ad3215e581bccdc110726fe45648ef8650
Abhinav Verma <abhinavv@gwu.edu>
2018-03-14 09:17:50 -0400

    Service Remoting
    Added service remoting to communicate between different services

--- ECommerce.ProductCatalog/ProductCatalog.cs
+++ ECommerce.ProductCatalog/ProductCatalog.cs
@@ -8,6 +8,8 @@ using ECommerce.Productcatalog;
 using ECommerce.Productcatalog.Model;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace ECommerce.ProductCatalog
@@ -15,12 +17,22 @@ namespace ECommerce.ProductCatalog
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class ProductCatalog : StatefulService
+    internal sealed class ProductCatalog : StatefulService, IProductCatalogService
     {
         private IProductRepository _repo;
         public ProductCatalog(StatefulServiceContext context)
             : base(context)
         { }
+
+        public async Task AddProduct(Product product)
+        {
+            await _repo.AddProduct(product);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _repo.GetAllProducts();
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -31,7 +43,7 @@ namespace ECommerce.ProductCatalog
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[0];
+            return  this.CreateServiceRemotingReplicaListeners<ProductCatalog>();
         }
 
         /// <summary>
--- ECommerse.API/Controllers/ProductsController.cs
+++ ECommerse.API/Controllers/ProductsController.cs
@@ -3,25 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Productcatalog.Model;
 using ECommerse.API.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
 
 namespace ECommerse.API.Controllers
 {
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
+        private readonly IProductCatalogService _catalogService;
+
+        public ProductsController()
+        {
+            _catalogService = ServiceProxy.Create<IProductCatalogService>(new Uri("fabric:/ECommerce/ProductCatalog"),
+                new Microsoft.ServiceFabric.Services.Client.ServicePartitionKey(0));
+        }
+
         [HttpGet]
         public async Task<IEnumerable<APIProduct>> Get()
         {
-            return new[] { new APIProduct {
-                Id = Guid.NewGuid(),
-                Name = "Fake",
-                Description = "Description of this product",
-                IsAvailable = true,
-                Price = 234
-            }
-            };
+            IEnumerable<Product> products = await _catalogService.GetAllProducts();
+            return products.Select(s => new APIProduct
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Description = s.Description,
+                IsAvailable = (s.Availability > 0),
+                Price = s.Price
+            });
+            
         }
     }
 }
